e41b820dfd8e75a8aa9e469a8028bdb8c96ba283
Robert Siński <75808735+Biplo12@users.noreply.github.com>
2023-09-02 15:47:54 +0200

    Initial commit from Create Next App

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+import Layout from '@/components/layout/Layout';
+import Seo from '@/components/Seo';
+
+export default function NotFoundPage() {
+  return (
+    <Layout>
+      <Seo templateTitle='Not Found' />
+    </Layout>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms')],
+} satisfies Config;
