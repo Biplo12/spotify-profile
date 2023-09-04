d477f83f47a8ed3e01049639ca82a3c5eeac18da
Robert Siński <75808735+Biplo12@users.noreply.github.com>
2023-09-04 19:39:35 +0200

    fix: responsive bugs

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -11,7 +11,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <>
       {isAuth && <Sidebar />}
       {children}
-      <Footer />
+      {isAuth && <Footer />}
     </>
   );
 }
